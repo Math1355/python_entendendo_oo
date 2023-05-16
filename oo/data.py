111d01b9800c9e3808f3e1b418e40524c55467d4
matheus <manijrprof@gmail.com>
2023-05-16 13:45:08 -0300

    Codigo fonte usado no curso entendendo OO com python

---
+++ oo/data.py
@@ -0,0 +1,10 @@
+
+class Data:
+
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def formatar_data(self):
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
